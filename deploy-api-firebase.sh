e6edb60ae153e8f4c6cb4646ef2f186ff9de7000
Will Beebe <wbeebe@google.com>
2020-06-06 17:08:33 -0700

    butler

--- deploy-api-firebase.sh
+++ deploy-api-firebase.sh
@@ -9,9 +9,11 @@ export FIREBASE_TOKEN=$3
 echo "token is $FIREBASE_TOKEN"
 
 cd $1
-npm install
-npm run "package-functions-$2"
-cd functions
-npm install
-cd ../
+yarn install
+yarn run "package-functions-$2"
+#cd functions
+#npm install
+#cd ../
+# can we run thi
+#yarn run "deploy-$2"
 firebase deploy --project $5 --only functions:$6
