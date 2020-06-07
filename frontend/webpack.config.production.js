d37ff5e4297095c3bc6bddb434e4c3e245708be6
Will Beebe <wbeebe@google.com>
2020-06-06 21:00:27 -0700

    not git

--- frontend/webpack.config.production.js
+++ frontend/webpack.config.production.js
@@ -1,11 +1,11 @@
 require('@babel/register');
 
 const webpack = require('webpack');
-const SentryCliPlugin = require('@sentry/webpack-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+// const SentryCliPlugin = require('@sentry/webpack-plugin');
+// const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const baseConfig = require('./webpack/base-config');
 
-const gitRevisionPlugin = new GitRevisionPlugin();
+// const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = {
   ...baseConfig,
@@ -15,15 +15,15 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
         NODE_BUILD_ENV: JSON.stringify('production'),
-        GIT_SHA: JSON.stringify(gitRevisionPlugin.commithash()),
-        RELEASE: JSON.stringify(`frontend-${gitRevisionPlugin.commithash()}`),
+        // GIT_SHA: JSON.stringify(gitRevisionPlugin.commithash()),
+        // RELEASE: JSON.stringify(`frontend-${gitRevisionPlugin.commithash()}`),
       },
     }),
-    new SentryCliPlugin({
-      configFile: '.sentryclirc',
-      include: '.',
-      ignore: ['node_modules', 'webpack.config.staging.js'],
-      release: `frontend-production-${gitRevisionPlugin.commithash()}`,
-    }),
+    // new SentryCliPlugin({
+    //   configFile: '.sentryclirc',
+    //   include: '.',
+    //   ignore: ['node_modules', 'webpack.config.staging.js'],
+    //   release: `frontend-production-${gitRevisionPlugin.commithash()}`,
+    // }),
   ],
 };
--- frontend/webpack.config.staging.js
+++ frontend/webpack.config.staging.js
@@ -1,11 +1,11 @@
 require('@babel/register');
 
 const webpack = require('webpack');
-const SentryCliPlugin = require('@sentry/webpack-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+// const SentryCliPlugin = require('@sentry/webpack-plugin');
+// const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const baseConfig = require('./webpack/base-config');
 
-const gitRevisionPlugin = new GitRevisionPlugin();
+// const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = {
   ...baseConfig,
@@ -15,15 +15,16 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
         NODE_BUILD_ENV: JSON.stringify('staging'),
-        GIT_SHA: JSON.stringify(gitRevisionPlugin.commithash()),
-        RELEASE: JSON.stringify(`frontend-${gitRevisionPlugin.commithash()}`),
+        // GIT_SHA: JSON.stringify(gitRevisionPlugin.commithash()),
+        // RELEASE: JSON.stringify(`frontend-${gitRevisionPlugin.commithash()}`),
       },
     }),
-    new SentryCliPlugin({
-      configFile: '.sentryclirc',
-      include: '.',
-      ignore: ['node_modules', 'webpack.config.staging.js'],
-      release: `frontend-staging-${gitRevisionPlugin.commithash()}`,
-    }),
+    // triggering from the git plugin isn't enough, it needs to be triggered from the CSR for git to work
+    // new SentryCliPlugin({
+    //   configFile: '.sentryclirc',
+    //   include: '.',
+    //   ignore: ['node_modules', 'webpack.config.staging.js'],
+    //   release: `frontend-staging-${gitRevisionPlugin.commithash()}`,
+    // }),
   ],
 };
